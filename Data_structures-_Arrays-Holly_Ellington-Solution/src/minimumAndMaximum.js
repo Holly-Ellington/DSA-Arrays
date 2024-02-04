2c7a919623933aef6e7a296e6589f7bc857d6b2c
Holly-Ellington <146004914+Holly-Ellington@users.noreply.github.com>
2024-02-03 20:17:12 -0600

    All Tests passing

---
+++ Data_structures-_Arrays-Holly_Ellington-Solution/src/minimumAndMaximum.js
@@ -0,0 +1,39 @@
+ 
+function minimumAndMaximumSort(numbers) {
+  const minMax = [];
+  
+   // Check if the array has at least one element
+  if (numbers.length >= 1) {
+   // Sort in ascending order
+
+    numbers.sort((a, b) => a - b);
+    minMax.push(numbers[0]);
+    minMax.push(numbers[numbers.length - 1]);
+  }
+  return minMax;
+}
+
+/**
+ * Implement an algorithm that uses iteration to find the minimum and maximum.
+ */
+function minimumAndMaximumIterate(numbers) {
+  if (numbers.length >= 1) {
+    let min = numbers[0];
+    let max = numbers[0];
+   // Iterate through each number in the array
+    numbers.forEach((number) => {
+      if (number < min) {
+        min = number;
+      }
+    // Update max if the current number is larger
+      if (number > max) {
+        max = number;
+      }
+    });
+    return [min, max];
+  }
+  return [];
+}
+
+module.exports = { minimumAndMaximumIterate, minimumAndMaximumSort };
+
